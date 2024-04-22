7c6d7a85f0bc7381dfc496fb0d15530a24c46a55
Hugo Cântara <hugo.cantara@gmail.com>
2024-04-22 17:13:13 +0100

    Commit - Create Mappers

---
+++ NatixisWebChat/NatixisWebChatInfrastructure/Mappers/AppMappers.cs
@@ -0,0 +1,16 @@
+﻿namespace NatixisWebChatInfrastructure.Mappers
+{
+    using AutoMapper;
+    using NatixisWebChatDomain.AppEntities;
+    using NatixisWebChatModels.AppModels;
+
+    public class AppMappers : Profile
+    {
+        public AppMappers()
+        {
+            CreateMap<UserEntity, UsersModel>().ReverseMap();
+
+            CreateMap<GroupEntity, GroupModel>().ReverseMap();
+        }
+    }
+}
